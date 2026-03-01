a78c9ff8306176e49839b23d66c873e4e4f16cdf
shreekar-reddy-sg <shreekarreddysg520@gmail.com>
2026-03-01 19:28:31 +0530

    User Model creation

---
+++ backend/models/user.model.js
@@ -0,0 +1,25 @@
+import mongoose, { Schema } from 'mongoose';
+
+const userSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    role: {
+        type: String,
+        default: 'member',
+    },
+}, {
+    timestamps: true,
+});
+    
+export const User = mongoose.model('User', userSchema);
