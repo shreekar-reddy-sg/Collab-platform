081a77d96ac56bd218c7aca667eb5649ca21a623
shreekar-reddy-sg <shreekarreddysg520@gmail.com>
2026-03-02 21:28:37 +0530

    Displayed messages after connection using socket.io

--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,35 @@
 import connectDB from './config/database.js';
 import dotenv from 'dotenv';
 import app from './app.js';
+import cors from 'cors';
+import http from 'http';
+import { Server } from 'socket.io';
+import express from 'express';
+
+app.use(cors());
+app.use(express.json());
 
 dotenv.config({ path: './.env' });
 
-const startServer = async () => {
-    try {
-        const port = process.env.PORT || 5000;  
-        await connectDB();
-        app.listen(port, () => {
-            console.log(`Server started on port ${port}`);
-        });
-    } catch (error) {
-        console.error('Some Error Occurred: ', error.message);
-        process.exit(1);
+const server = http.createServer(app);
+const io = new Server(server, {
+    cors: {
+        origin: 'http://localhost:3000'
     }
-};
+});
 
-startServer();
+io.on('connection', (socket) => {
+    console.log('A user connected: ', socket.id);
+    socket.on("send_message", (data) => {
+        console.log("Message received: ", data);
+        io.emit("receive_message", data);
+    });
+    socket.on('disconnect', () => {
+        console.log('A user disconnected: ', socket.id);
+    });
+});
+
+server.listen(process.env.PORT, () => {
+    console.log(`Server is running on port ${process.env.PORT}`);
+    connectDB();
+});
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,43 @@
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
+import { io } from "socket.io-client";
+
+const socket = io("http://localhost:5000");
 
 function App() {
+  const [message, setMessage] = useState("");
+  const [messages, setMessages] = useState([]);
+
   useEffect(() => {
-    fetch("http://localhost:5000/")
-      .then(res => res.text())
-      .then(data => console.log(data));
+    socket.on("connect", () => {
+      console.log("Connected to server with ID: ", socket.id);
+    });
+    socket.on("receive_message", (data) => {
+      setMessages((prev) => [...prev, data]);
+    });
   }, []);
 
-  return <h1>Frontend Connected</h1>;
+  const sendMessage = () => {
+    socket.emit("send_message", message);
+    setMessage("");
+  };
+
+  return (
+    <div>
+      <h2>Real Time Chat</h2>
+
+      <input
+        value={message}
+        onChange={(e) => setMessage(e.target.value)}
+      />
+      <button onClick={sendMessage}>Send</button>
+
+      <div>
+        {messages.map((msg, index) => (
+          <p key={index}>{msg}</p>
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default App;
