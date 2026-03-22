93aca47840093ec23c8810609170baedaa92013b
shreekar-reddy-sg <shreekarreddysg520@gmail.com>
2026-03-22 21:06:41 +0530

    Used nginx as proxy instead of ports

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:5000");
+const socket = io();
 
 function App() {
   const [message, setMessage] = useState("");
